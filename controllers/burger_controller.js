5fce01732ed196a6fc08df8bcbee5a3baa50fed6
Nina Koller <nckoller@gmail.com>
2020-10-09 21:37:58 -0500

    added burger_controllers routes

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,6 +1,46 @@
-import express from "express";
-import burger from "./models/burger.js";
+import express from 'express';
+import burger from './models/burger.js';
 
-// Router
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  burger.all(function (data) {
+    const handlebarsObj = {
+      cats: data,
+    };
+    console.log(handlebarsObj);
+    res.render('index', handlebarsObj);
+  });
+});
+
+router.post('/api/burgers', function (req, res) {
+  burger.create(
+    ['burger_name', 'devoured'],
+    [req.body.burger_name, req.body.devoured],
+    function (result) {
+      res.json({ id: result.insertId });
+    }
+  );
+});
+
+router.put('/api/burgers/:id', function (req, res) {
+  var condition = 'id = ' + req.params.id;
+
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    function (result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
 
 module.exports = router;
