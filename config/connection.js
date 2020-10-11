42ccc0e5be1d919e378e7988a5020b9f11acd61a
Nina Koller <nckoller@gmail.com>
2020-10-10 22:55:39 -0500

    added css, added jawsdb to connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 const mysql = require("mysql");
 const { user, password } = require("./config");
-const connection = mysql.createConnection({
+
+if (process.env.JAWSDB_URL) {
+  const connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
   host: "localhost",
   // Your port if not 3306
   port: 3306,
@@ -14,5 +18,6 @@ connection.connect(function (err) {
   if (err) throw err;
   //   console.log("connected as id " + connection.threadId + "\n");
 });
+}
 
 module.exports = connection;
