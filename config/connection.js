55d9e562a89f08f3877664cb50b742d2a6972ee0
Nina Koller <nckoller@gmail.com>
2020-10-10 23:50:26 -0500

    fixing connection issue for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,25 @@
-const mysql = require("mysql");
-// const { user, password } = require("./config");
+const mysql = require('mysql');
+// const { user, password } = require('./config');
 
+// User JAWSDB if Heroku detects it, otherwise use localhost
 if (process.env.JAWSDB_URL) {
   const connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
- connection = mysql.createConnection({
-  host: "localhost",
-  // Your port if not 3306
-  port: 3306,
-  // Your username
-  user: "root",
-  // Your password
-  password:"DUmn612Hcd",
-  database: "burgers_db",
-});
+  connection = mysql.createConnection({
+    host: 'localhost',
+    // Your port if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "DUmn612Hcd",
+    database: 'burgers_db',
+  });
+}
+// Make connection
 connection.connect(function (err) {
   if (err) throw err;
   //   console.log("connected as id " + connection.threadId + "\n");
 });
-}
 
 module.exports = connection;
